3ffb5d9c61c6257dcff90064cf0ea4e207de8cce
Kento Kuga <kento.kuga.hidari@gmail.com>
2021-03-06 00:56:01 +0900

    feat : fetch github api button

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("index.js: loaded");
+
+// http通信関数
+const fetchUserInfo = (userId) => {
+  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then((res) => {
+      console.log(res.status);
+
+      if (res.ok) {
+        return res.json().then((userInfo) => {
+          console.log(userInfo);
+        });
+      } else {
+        console.error("エラーレスポンス", res);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
